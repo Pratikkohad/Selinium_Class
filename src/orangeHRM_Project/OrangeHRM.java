d0f0b0656f36e3ed95df24f49a14a3a83d6f0937
Pratik Kohad <kohad.1995@gmail.com>
2021-05-08 19:28:44 +0530

    selenium

--- src/orangeHRM_Project/OrangeHRM.java
+++ src/orangeHRM_Project/OrangeHRM.java
@@ -6,7 +6,8 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.Select;
 
-public class OrangeHRM {
+public class OrangeHRM
+{
 	static WebDriver driver;
 	public static void main(String[] args) throws InterruptedException 
 	{
